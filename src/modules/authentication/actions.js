d4fa8b97ec43e2b74ef3152186d93013286662ad
Juanma Cano <juan.cano.external@atos.net>
2020-08-06 09:38:44 +0200

    build

--- src/modules/authentication/actions.js
+++ src/modules/authentication/actions.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
 
 export async function signIn(context, user) {
+
+  context.commit("setLoading", true, { root: true });
+  if (user.email === "admin@vue.com" && user.password === "@Password1") context.commit("setUser", user)
+  else context.commit("authError", "invalid credentials");
+  context.commit("setLoading", false, { root: true });
+}
+
+
+
+// OPTION using FAKE API with json-server
+// reading json-server/login.js
+export async function signInAPI(context, user) {
   try {
     context.commit("setLoading", true, { root: true });
     await Vue.axios({
@@ -15,3 +27,4 @@ export async function signIn(context, user) {
     context.commit("setLoading", false, { root: true });
   }
 }
+
